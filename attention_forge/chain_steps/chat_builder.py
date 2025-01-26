87891485407d4ae908f66d8403528699bcd93aef
smith <smith@attentionforge>
2025-01-25 17:02:11 -0800

    make it possible to use base client and model in a chain config

--- attention_forge/chain_steps/chat_builder.py
+++ attention_forge/chain_steps/chat_builder.py
@@ -1,5 +1,6 @@
 from attention_forge.chain_steps.chat import Chat
 from attention_forge.chain_steps.chat_logger import ChatLogger
+import sys
 
 class ChatBuilder:
     def __init__(self, api_key, role_handler, context_files, project_config):
@@ -10,8 +11,25 @@ class ChatBuilder:
         self.chat_logger = ChatLogger(self.project_config.get("log_file", "chat_log.txt"))
 
     def build(self, role_name, step_config):
-        client = step_config.get("client", self.project_config.get("client", "openai"))
-        model = step_config.get("model", self.project_config.get("model", ""))
+        client_value = step_config.get("client", self.project_config.get("client", "openai"))
+        if client_value == "base_client":
+            if "base_client" in self.project_config:
+                client = self.project_config["base_client"]
+            else:
+                print("Error: 'base_client' is not defined in the project configuration.")
+                sys.exit(1)
+        else:
+            client = client_value
+
+        model_value = step_config.get("model", self.project_config.get("model", ""))
+        if model_value == "base_model":
+            if "base_model" in self.project_config:
+                model = self.project_config["base_model"]
+            else:
+                print("Error: 'base_model' is not defined in the project configuration.")
+                sys.exit(1)
+        else:
+            model = model_value
 
         return Chat(
             self.api_key,
